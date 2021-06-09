0539e89ea5a78f0a7794b5ef20383bb3661b2eb3
Cristian Pinzon <cristianpinzon97@hotmail.com>
2021-06-08 22:57:24 -0500

    changing some configuration files for Heroku

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/GhibliFilmsApp'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/GhibliFilmsApp'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/GhibliFilmsApp/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/GhibliFilmsApp/' }),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
